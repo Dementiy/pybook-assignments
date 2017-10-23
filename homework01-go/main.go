c82b76fac56428acbbc64927d3cba8071b634417
dementiy <Dementiy@yandex.ru>
2017-10-23 17:14:53 +0300

    Добавлен пример работы с RSA

---
+++ homework01-go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "os"
+    "./rsa"
+)
+
+
+func main() {
+    p := flag.Int("p", 11, "")
+    q := flag.Int("q", 17, "")
+    plaintext := flag.String("text", "", "")
+    flag.Parse()
+    keys, err := rsa.GenerateKeypair(*p, *q)
+    fmt.Println(keys)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    ciphertext := rsa.Encrypt(keys.Private, *plaintext)
+    fmt.Println("Encrypted message:", ciphertext)
+    fmt.Println("Decrypted message:", rsa.Decrypt(keys.Public, ciphertext))
+}
