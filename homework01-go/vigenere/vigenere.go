9a09ff187dd4d4171c7341729af514ca87d9fec7
dementiy <Dementiy@yandex.ru>
2017-10-23 15:47:42 +0300

    Добавлен шаблон для шифра Виженера

---
+++ homework01-go/vigenere/vigenere.go
@@ -0,0 +1,18 @@
+package vigenere
+
+func encrypt_vigenere(plaintext string, keyword string) string {
+    var ciphertext string
+
+    // PUT YOUR CODE HERE
+
+    return ciphertext
+}
+
+func decrypt_vigenere(ciphertext string, keyword string) string {
+    var plaintext string
+
+    // PUT YOUR CODE HERE
+
+    return plaintext
+}
+
