7cd7d112dd58dcb2dc06542a50873575d2767d3d
dementiy <Dementiy@yandex.ru>
2017-10-27 16:46:52 +0300

    Добавлен шаблон для второй части игры Жизнь

---
+++ homework03/life_with_classes.py
@@ -0,0 +1,97 @@
+import pygame
+from pygame.locals import *
+import random
+from copy import deepcopy
+
+
+class GameOfLife:
+
+    def __init__(self, width=640, height=480, cell_size=10, speed=10):
+        self.width = width
+        self.height = height
+        self.cell_size = cell_size
+
+        # Устанавливаем размер окна
+        self.screen_size = width, height
+        # Создание нового окна
+        self.screen = pygame.display.set_mode(self.screen_size)
+
+        # Вычисляем количество ячеек по вертикали и горизонтали
+        self.cell_width = self.width // self.cell_size
+        self.cell_height = self.height // self.cell_size
+
+        # Скорость протекания игры
+        self.speed = speed
+
+    def draw_grid(self):
+        """ Отрисовать сетку """
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                    (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                    (0, y), (self.width, y))
+
+    def run(self):
+        """ Запустить игру """
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Game of Life')
+        self.screen.fill(pygame.Color('white'))
+
+        # Создание списка клеток
+        # PUT YOUR CODE HERE
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+            self.draw_grid()
+
+            # Отрисовка списка клеток
+            # Выполнение одного шага игры (обновление состояния ячеек)
+            # PUT YOUR CODE HERE
+
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+
+
+class Cell:
+
+    def __init__(self, row, col, state=False):
+        pass
+
+    def is_alive(self):
+        pass
+
+
+class CellList:
+
+    def __init__(self, nrows, ncols, randomize=False):
+        pass
+
+    def get_neighbours(self, cell):
+        neighbours = []
+        # PUT YOUR CODE HERE
+        return neighbours
+
+    def update(self):
+        new_clist = deepcopy(self)
+        # PUT YOUR CODE HERE
+        return self
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    @classmethod
+    def from_file(cls, filename):
+        pass
+
