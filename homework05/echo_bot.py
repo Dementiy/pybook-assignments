f0f92db34910ff16693159d58ca6b03cfa2cf83f
dementiy <Dementiy@yandex.ru>
2017-11-16 20:56:57 +0300

    Добавлен пример простого echo-бота

---
+++ homework05/echo_bot.py
@@ -0,0 +1,13 @@
+import telebot
+
+
+access_token = 'YOUR TOKEN'
+bot = telebot.TeleBot(access_token)
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.polling(non_stop=True)
+
