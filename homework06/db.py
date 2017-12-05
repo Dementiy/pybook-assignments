0b83be55a566b6aad789b6f0e8f73ea5ad84c301
dementiy <Dementiy@yandex.ru>
2017-12-05 18:28:26 +0300

    Add database template

---
+++ homework06/db.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+engine = create_engine("sqlite:///news.db")
+session = sessionmaker(bind=engine)
+
+
+class News(Base):
+    __tablename__ = "news"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    url = Column(String)
+    comments = Column(Integer)
+    points = Column(Integer)
+    label = Column(String)
+
+Base.metadata.create_all(bind=engine)
