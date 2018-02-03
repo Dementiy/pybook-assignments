9d7a23c3844fc0769e702a89ecdf9fbc5a526cb3
dementiy <Dementiy@yandex.ru>
2018-02-03 19:05:53 +0300

    Кастомизация отображения заметок

--- homework07/elevennote/src/notes/admin.py
+++ homework07/elevennote/src/notes/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Note
 
-admin.site.register(Note)
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+admin.site.register(Note, NoteAdmin)
