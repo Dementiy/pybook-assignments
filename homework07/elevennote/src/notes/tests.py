774cb7c076880e9291ddad09e86814920ba0fd97
dementiy <Dementiy@yandex.ru>
2018-02-03 17:06:40 +0300

    Добавлен тест для модели заметки

--- homework07/elevennote/src/notes/tests.py
+++ homework07/elevennote/src/notes/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from notes.models import Note
+
+
+class TestNoteModel(TestCase):
+
+    def test_can_create_a_new_note(self):
+        note = Note.objects.create(title='Note title', body='Note body')
+        self.assertTrue(note)
+
+    def test_string_representation(self):
+        note = Note.objects.create(title='Note title', body='Note body')
+        self.assertEqual(str(note), 'Note title')
+
