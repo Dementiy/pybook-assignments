ad1f97b7a61c3e804142ec6a18228ff63d665a9f
dementiy <Dementiy@yandex.ru>
2018-02-03 17:06:16 +0300

    Добавлена модель заметки

--- homework07/elevennote/src/notes/models.py
+++ homework07/elevennote/src/notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
