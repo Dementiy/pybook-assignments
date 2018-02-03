9b92b4f05690056c55b1468a6f18fbd0d4714e84
dementiy <Dementiy@yandex.ru>
2018-02-03 18:59:13 +0300

    Добавлен метод проверки даты публикации заметки

--- homework07/elevennote/src/notes/models.py
+++ homework07/elevennote/src/notes/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - timezone.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.title
