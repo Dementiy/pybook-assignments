910d4201b1d3cce41938e4034627dcd296cc3561
dementiy <Dementiy@yandex.ru>
2018-02-03 15:14:40 +0300

    Разделение конфигурации проекта на base, local и production

---
+++ homework07/elevennote/src/config/settings/local.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'django.contrib.postgres',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
+    }
+}
