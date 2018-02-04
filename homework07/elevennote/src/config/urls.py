f52577b31a5a00d3cb5027296cf3b3726718b16f
dementiy <Dementiy@yandex.ru>
2018-02-04 16:19:00 +0300

    Добавлено имя для главной страницы

--- homework07/elevennote/src/config/urls.py
+++ homework07/elevennote/src/config/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='notes/')),
+    path('', RedirectView.as_view(url='notes/'), name='index'),
     path('notes/', include('notes.urls', namespace='notes')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('admin/', admin.site.urls),
