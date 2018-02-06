ca80815c5f215726d3a6a2cdf5acd655c44007ff
dementiy <Dementiy@yandex.ru>
2018-02-06 19:34:45 +0300

    Использование LoginRequiredMixin вместо декоратора

--- homework07/elevennote/src/notes/views.py
+++ homework07/elevennote/src/notes/views.py
@@ -1,31 +1,22 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Note
 
 
-class NoteList(ListView):
+class NoteList(LoginRequiredMixin, ListView):
     paginate_by = 5
     template_name = 'notes/index.html'
     context_object_name = 'latest_note_list'
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(NoteList, self).dispatch(*args, **kwargs)
 
     def get_queryset(self):
         return Note.objects.filter(owner=self.request.user).order_by('-pub_date')
 
 
-class NoteDetail(DetailView):
+class NoteDetail(LoginRequiredMixin, DetailView):
     model = Note
     template_name = 'notes/detail.html'
     context_object_name = 'note'
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(NoteDetail, self).dispatch(*args, **kwargs)
 
     def get_queryset(self):
         return Note.objects.filter(owner=self.request.user)
