ff2919a4f2b8452e452d58bc9bf6a91d098bc8a4
kuraiakari <55931801+kuraiakari@users.noreply.github.com>
2019-10-06 03:36:50 +0700

    Update vigenere.go

--- homework01-go/vigenere/vigenere.go
+++ homework01-go/vigenere/vigenere.go
@@ -4,13 +4,47 @@ func EncryptVigenere(plaintext string, keyword string) string {
 	var ciphertext string
 
 	// PUT YOUR CODE HERE
-
+    ciphertext=str()
+    tro=0
+    for i in range(0,len(plaintext)):
+        if plaintext[i] >= 'A':
+            if plaintext[i]<='Z':
+                t=ord(plaintext[i])+ord(keyword[tro])-ord('A')
+                if t > ord('Z'):
+                    t-=26
+                ciphertext+=chr(t)
+        if plaintext[i] >= 'a':
+            if plaintext[i]<='z':
+                t=ord(plaintext[i])+ord(keyword[tro])-ord('a')
+                if t > ord('z'):
+                    t-=26
+                ciphertext+=chr(t)
+        tro+=1
+        if tro== len(keyword):
+            tro=0
 	return ciphertext
 }
 
 func DecryptVigenere(ciphertext string, keyword string) string {
 	var plaintext string
-
+    plaintext=str()
+    tro=0
+    for i in range(0,len(ciphertext)):
+        if ciphertext[i] >= 'A':
+            if ciphertext[i]<='Z':
+                t=ord(ciphertext[i])-ord(keyword[tro])+ord('A')
+                if t < ord('A'):
+                    t+=26
+                plaintext+=chr(t)
+        if ciphertext[i] >= 'a':
+            if ciphertext[i]<='z':
+                t=ord(sciphertext[i])-ord(keyword[tro])+ord('a')
+                if t < ord('a'):
+                    t+=26
+                plaintext+=chr(t)
+        tro+=1
+        if tro== len(keyword) :
+            tro=0
 	// PUT YOUR CODE HERE
 
 	return plaintext
