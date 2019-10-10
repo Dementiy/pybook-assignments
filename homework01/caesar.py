1a148b78facc0e319a6e4fc105c71264e21b7381
kuraiakari <thienbinh04102000@gmail.com>
2019-10-11 04:40:11 +0700

    solve task-1

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,4 @@
-def encrypt_caesar(plaintext):
+def encrypt_caesar(s):
     """
     Encrypts plaintext using a Caesar cipher.
 
@@ -12,10 +12,30 @@ def encrypt_caesar(plaintext):
     ''
     """
     # PUT YOUR CODE HERE
+    ciphertext=str()
+    for i in range (0,len(s)):
+        t = int(ord(s[i]))
+        if t <ord('A'):
+            ciphertext+=chr(t)
+            continue
+        if t >ord('Z'):
+            if t<ord('a'):
+                ciphertext+=chr(t)
+                continue
+        if t>ord('z'):
+            ciphertext+=chr(t)
+            continue
+        t+=3
+        if ( t > ord('Z') ):
+            if( t < ord('a') ):
+                t=t-ord('Z')+ord('A')-1
+            if( t >= ord('z') ):
+                t=t-ord('z')+ord('a')-1
+        ciphertext+=chr(t)
     return ciphertext
 
 
-def decrypt_caesar(ciphertext):
+def decrypt_caesar(s):
     """
     Decrypts a ciphertext using a Caesar cipher.
 
@@ -29,4 +49,31 @@ def decrypt_caesar(ciphertext):
     ''
     """
     # PUT YOUR CODE HERE
-    return plaintext
+    s1=str()
+    for i in range (0,len(s)):
+        t = int(ord(s[i]))
+        if t <ord('A'):
+            s1+=chr(t)
+            continue
+        if t >ord('Z'):
+            if t<ord('a'):
+                s1+=chr(t)
+                continue
+        if t>ord('z'):
+            s1+=chr(t)
+            continue
+        t-=3
+        if ( t < ord('A') ):
+            t=t+ord('Z')-ord('A')+1
+        if( t < ord('a') ):
+            if ( t > ord('Z')):
+                t=t+ord('z')-ord('a')+1
+        s1+=chr(t)
+    return s1
+
+plaintext=str(input())
+ciphertext=str(input())
+kq1=encrypt_caesar(plaintext)
+print(kq1)
+kq2=decrypt_caesar(ciphertext)
+print(kq2)
