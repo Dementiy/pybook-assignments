3686b2d258f452bfd47057e6d1b657a59ccd2ef8
Dementiy <Dementiy@yandex.ru>
2020-08-12 23:07:33 +0300

    Обновлены тесты для шифра Цезаря

--- homework01/tests/test_caesar.py
+++ homework01/tests/test_caesar.py
@@ -1,36 +1,59 @@
 import unittest
-from solutions import caesar
+
+import caesar
 
 
-class CaesarCipherTest(unittest.TestCase):
+class EncryptTestCase(unittest.TestCase):
+    def test_shift0(self):
+        cases = [
+            ("", ""),
+            ("python", "python"),
+            ("PYTHON", "PYTHON"),
+            ("Python", "Python"),
+            ("Python3.6", "Python3.6"),
+        ]
 
-    def test_encrypt_empty_string(self):
-        self.assertEqual(caesar.encrypt_caesar(""), "")
+        for i, (plaintext, chiphertext) in enumerate(cases):
+            with self.subTest(case=i, plaintext=plaintext, chiphertext=chiphertext):
+                self.assertEqual(chiphertext, caesar.encrypt_caesar(plaintext, shift=0))
 
-    def test_encrypt_uppercase(self):
-        self.assertEqual(caesar.encrypt_caesar("PYTHON"), "SBWKRQ")
+    def test_shift3(self):
+        cases = [
+            ("", ""),
+            ("PYTHON", "SBWKRQ"),
+            ("python", "sbwkrq"),
+            ("Python", "Sbwkrq"),
+            ("Python3.6", "Sbwkrq3.6"),
+        ]
 
-    def test_encrypt_lowercase(self):
-        self.assertEqual(caesar.encrypt_caesar("python"), "sbwkrq")
+        for i, (plaintext, chiphertext) in enumerate(cases):
+            with self.subTest(case=i, plaintext=plaintext, chiphertext=chiphertext):
+                self.assertEqual(chiphertext, caesar.encrypt_caesar(plaintext, shift=3))
 
-    def test_encrypt_uppercase_and_lowercase(self):
-        self.assertEqual(caesar.encrypt_caesar("Python"), "Sbwkrq")
 
-    def test_encrypt_uppercase_lowercase_and_digits(self):
-        self.assertEqual(caesar.encrypt_caesar("Python3.6"), "Sbwkrq3.6")
+class DecryptTestCase(unittest.TestCase):
+    def test_shift0(self):
+        cases = [
+            ("", ""),
+            ("python", "python"),
+            ("PYTHON", "PYTHON"),
+            ("Python", "Python"),
+            ("Python3.6", "Python3.6"),
+        ]
 
-    def test_decrypt_empty_string(self):
-        self.assertEqual(caesar.decrypt_caesar(""), "")
+        for i, (chiphertext, plaintext) in enumerate(cases):
+            with self.subTest(case=i, chiphertext=chiphertext, plaintext=plaintext):
+                self.assertEqual(plaintext, caesar.decrypt_caesar(chiphertext, shift=0))
 
-    def test_decrypt_uppercase(self):
-        self.assertEqual(caesar.decrypt_caesar("SBWKRQ"), "PYTHON")
+    def test_shift3(self):
+        cases = [
+            ("", ""),
+            ("SBWKRQ", "PYTHON"),
+            ("sbwkrq", "python"),
+            ("Sbwkrq", "Python"),
+            ("Sbwkrq3.6", "Python3.6"),
+        ]
 
-    def test_decrypt_lowercase(self):
-        self.assertEqual(caesar.decrypt_caesar("sbwkrq"), "python")
-
-    def test_decrypt_uppercase_and_lowercase(self):
-        self.assertEqual(caesar.decrypt_caesar("Sbwkrq"), "Python")
-
-    def test_decrypt_uppercase_lowercase_and_digits(self):
-        self.assertEqual(caesar.decrypt_caesar("Sbwkrq3.6"), "Python3.6")
-
+        for i, (chiphertext, plaintext) in enumerate(cases):
+            with self.subTest(case=i, chiphertext=chiphertext, plaintext=plaintext):
+                self.assertEqual(plaintext, caesar.decrypt_caesar(chiphertext, shift=3))
