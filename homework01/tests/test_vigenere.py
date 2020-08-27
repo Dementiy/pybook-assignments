b25a724a92512623020eb440f33f7730f63cd573
Dementiy <Dementiy@yandex.ru>
2020-08-28 00:52:43 +0300

    Обновлен тест

--- homework01/tests/test_vigenere.py
+++ homework01/tests/test_vigenere.py
@@ -1,3 +1,5 @@
+import random
+import string
 import unittest
 
 import vigenere
@@ -31,3 +33,10 @@ class VigenereTestCase(unittest.TestCase):
                 case=i, chiphertext=chiphertext, keyword=keyword, plaintext=plaintext
             ):
                 self.assertEqual(plaintext, vigenere.decrypt_vigenere(chiphertext, keyword))
+
+    def test_randomized(self):
+        kwlen = random.randint(4, 24)
+        keyword = ''.join(random.choice(string.ascii_letters) for _ in range(kwlen))
+        plaintext = ''.join(random.choice(string.ascii_letters + ' -,') for _ in range(64))
+        ciphertext = vigenere.encrypt_vigenere(plaintext, keyword)
+        self.assertEqual(plaintext, vigenere.decrypt_vigenere(ciphertext, keyword))
