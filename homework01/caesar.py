52437ef8fbd12b5c9c5627c6e184901bdaafaf3d
rendex85 <rendex85@gmail.com>
2020-10-13 19:43:59 +0300

    caesar done

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -4,7 +4,6 @@ import typing as tp
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
-
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -14,8 +13,23 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
+
     ciphertext = ""
     # PUT YOUR CODE HERE
+    for i in plaintext:
+        try:
+            num = int(i)
+            ciphertext += str(num)
+        except ValueError:
+            if ord(i) in range(65, 91):
+                newchar = ((ord(i) - 64 + shift) % 26) + 64
+                ciphertext += chr(newchar)
+
+            elif ord(i) in range(97, 122):
+                newchar = ((ord(i) - 96 + shift) % 26) + 96
+                ciphertext += chr(newchar)
+            else:
+                ciphertext += i
     return ciphertext
 
 
@@ -34,6 +48,28 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    for i in ciphertext:
+        try:
+            num = int(i)
+            plaintext += str(num)
+        except ValueError:
+            if ord(i) in range(65, 91):
+                newchar = ((ord(i) - 64 - shift) % 26)
+                if newchar <= 0:
+                    newchar=(26-newchar)+64
+                else:
+                    newchar+=64
+                plaintext += chr(newchar)
+
+            elif ord(i) in range(97, 122):
+                newchar = ((ord(i) - 96 - shift) % 26)
+                if newchar <= 0:
+                    newchar = (26 - newchar) + 96
+                else:
+                    newchar += 96
+                plaintext += chr(newchar)
+            else:
+                plaintext += i
     return plaintext
 
 
@@ -44,3 +80,5 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
     # PUT YOUR CODE HERE
     return best_shift
+
+print(decrypt_caesar(""))
