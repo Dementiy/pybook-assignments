eb0c7fd51dbcdde59c7e7d7e05ac6187f1ff945e
Dementiy <Dementiy@yandex.ru>
2020-12-02 19:57:08 +0300

    Добавлена 0-ая практическая

---
+++ homework00/hello.py
@@ -0,0 +1,7 @@
+def get_greeting(name: str) -> str:
+    pass
+
+
+if __name__ == "__main__":
+    message = get_greeting("World")
+    print(message)
---
+++ homework00/test_hello.py
@@ -0,0 +1,16 @@
+import io
+import unittest
+from unittest.mock import patch
+
+from hello import get_greeting
+
+
+class HelloTestCase(unittest.TestCase):
+    def test_hello(self):
+        cases = [
+            ("World", "Hello, World!"),
+            ("Anonymous", "Hello, Anonymous!"),
+        ]
+        for i, (name, message) in enumerate(cases, start=1):
+            with self.subTest(case=i):
+                self.assertEqual(message, get_greeting(name))
